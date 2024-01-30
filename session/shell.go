mendersoftware/mender-connect

fix: Stop shell after health check fails
`mender-connect` has two levels of ping/pong, the standard websockets
one and a custom `shell` protocol one. On the later, when the peer does
not respond to the ping request the shell session, the session is marked
as expired but there was no default mechanism to stop the shell and get
ready for new shells.

The symptom of this bug is that after temporally lose of connectivity,
`mender-connect` will still refuse to open new shells after the
connectivity is restored.

It was possible to configure `mender-connect` to stop shells from expired
sessions. See changelog entry below.

Ticket: MEN-6888
Changelog: Stop shell after the ping/pong health check fails, so that
the `mender-connect` can recover after disruption of connectivity.
For already released versions of `mender-connect` the workaround is to
force the stop of shells for expired sessions adding to the
configuration options the fields:
```
  "Sessions": {
    "StopExpired": true,
    "ExpireAfterIdle": 600
  }
```
Setting a limit for expiration high enough (10 minutes, above) so that it
doesn't interfere with regular operations.
See https://docs.mender.io/add-ons/mender-connect#remote-terminal-configuration

Signed-off-by: Lluis Campos <lluis.campos@northern.tech>

session/shell.go
@@ -212,6 +212,18 @@ func MenderShellSessionsGetByUserId(userId string) []*MenderShellSession {
 	}
 }
 
+func MenderShellStopById(sessionId string) error {
+	s := MenderShellSessionGetById(sessionId)
+	if s.shell == nil {
+		return ErrSessionNotFound
+	}
+	e := s.StopShell()
+	if e != nil && procps.ProcessExists(s.shellPid) {
+		return e
+	}
+	return MenderShellDeleteById(sessionId)
+}
+
 func MenderShellStopByUserId(userId string) (count uint, err error) {
 	a := sessionsByUserIdMap[userId]
 	log.Debugf("stopping all shells of user %s.", userId)
@@ -396,6 +408,12 @@ func (s *MenderShellSession) healthcheck() {
 			if s.healthcheckTimeout.Before(time.Now()) {
 				log.Errorf("session %s, health check failed, connection with the client lost", s.id)
 				s.expiresAt = time.Now()
+				err := MenderShellStopById(s.id)
+				if err != nil {
+					log.Errorf("session %s, error stopping shell %s", s.id, err.Error())
+				} else {
+					log.Infof("session %s successfully stopped", s.id)
+				}
 				return
 			}
 		case <-time.After(time.Until(nextHealthcheckPing)):

alfrunes/nt-connect

(cherry picked from commit mendersoftware/mender-connect@e337160501a5f2c4278b98927ba828945c8bf2f2)

session/shell.go
@@ -202,6 +202,17 @@ func GetSessionsByUserId(userId string) []*TerminalSession {
 		return nil
 	}
 }
+func StopSessionById(sessionId string) error {
+	s := GetSessionById(sessionId)
+	if s.shell == nil {
+		return ErrSessionNotFound
+	}
+	e := s.StopShell()
+	if e != nil && procps.ProcessExists(s.shellPid) {
+		return e
+	}
+	return DeleteSessionById(sessionId)
+}
 
 func StopSessionByUserId(userId string) (count uint, err error) {
 	a := sessionsByUserIdMap[userId]
@@ -388,6 +399,12 @@ func (s *TerminalSession) healthcheck() {
 			if s.healthcheckTimeout.Before(time.Now()) {
 				log.Errorf("session %s, health check failed, connection with the client lost", s.id)
 				s.expiresAt = time.Now()
+				err := StopSessionById(s.id)
+				if err != nil {
+					log.Errorf("session %s, error stopping shell %s", s.id, err.Error())
+				} else {
+					log.Infof("session %s successfully stopped", s.id)
+				}
 				return
 			}
 		case <-time.After(time.Until(nextHealthcheckPing)):